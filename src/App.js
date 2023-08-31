0df07a8b56f777e9689819d3a6669bf31d030b55
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2023-08-31 07:04:14 +0100

    Initial commit: Added assets and improved project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        INSTAGRAM UI CLONE USING REACT JS 🤣 FOR BEGINNER
+      </header>
+    </div>
+  );
+}
+
+export default App;
